1713ab5cd8b176af3dfcad0c752b00508285b151
Danilo Santos <danilosantos.itech@gmail.com>
2023-01-06 21:14:32 -0300

    Trabalhando com data no javascript

--- cypress/integration/dash.spec.js
+++ cypress/integration/dash.spec.js
@@ -11,7 +11,7 @@ describe('dashboard', function () {
 
             },
 
-            samurai: {
+            provider: {
                 name: 'Ramon Valdes',
                 email: 'ramon@televisa.com',
                 password: 'pwd123',
@@ -20,21 +20,62 @@ describe('dashboard', function () {
         }
 
         before(function(){
+            cy.postUser(data.provider)
             cy.postUser(data.customer)
-            cy.postUser(data.samurai)
+            
 
             cy.apiLogin(data.customer)
             cy.log('Conseguimos pegar o token' + Cypress.env('apiToken'))
+            cy.setProviderId(data.provider.email)
         })
 
 
 
         it('o mesmo deve ser exibido no dashboard', function(){
 
-            console.log(data)
+            cy.log(' O Id do Ramom é ' + Cypress.env('providerId') )
+            cy.createAppointment()
         })
     })
 })
+
+import moment from 'moment'
+
+Cypress.Commands.add('createAppointment', function(){
+     let now = new Date()
+
+     now.setDate(now.getDate() + 1 )
+     const day = moment(now).format('YYYY-MM-DD  14:00:00 ')
+
+     cy.log(day)
+})
+
+
+
+
+
+Cypress.Commands.add('setProviderId', function(providerEmail){
+    cy.request({
+        method: 'GET',
+        url: 'http://localhost:3333/providers',
+        headers: {
+            authorization: 'Bearer ' + Cypress.env('apiToken')
+        }
+    }).then(function(response){
+        expect(response.status).to.eq(200)
+        console.log(response.body)
+
+        const providerList = response.body
+
+        providerList.forEach(function(provider){
+            if (provider.email === providerEmail){
+                Cypress.env('providerId', provider.id)
+            }
+
+        })
+    })
+})
+
 
 
 Cypress.Commands.add('apiLogin',function(user){
