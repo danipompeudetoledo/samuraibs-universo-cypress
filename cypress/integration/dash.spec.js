92c47518855ffb9db7e1f11e4b1bf741c7ec2849
Danilo Santos <danilosantos.itech@gmail.com>
2023-01-13 17:20:32 -0300

    Ajustes importantes

--- cypress/integration/dash.spec.js
+++ cypress/integration/dash.spec.js
@@ -1,4 +1,5 @@
 import loginPage from '../support/pages/login'
+import dashPage from '../support/pages/dash'
 
 describe('dashboard', function () {
 
@@ -18,7 +19,9 @@ describe('dashboard', function () {
                 email: 'ramon@televisa.com',
                 password: 'pwd123',
                 is_provider: true
-            }
+            },
+
+            appointmentHour: '14:00'
         }
 
         before(function(){
@@ -27,9 +30,8 @@ describe('dashboard', function () {
             
 
             cy.apiLogin(data.customer)
-            cy.log('Conseguimos pegar o token' + Cypress.env('apiToken'))
             cy.setProviderId(data.provider.email)
-            cy.createAppointment()
+            cy.createAppointment(data.appointmentHour)
         })
 
 
@@ -40,7 +42,14 @@ describe('dashboard', function () {
             loginPage.form(data.provider)
             loginPage.submit()
 
-            cy.wait(3000)
+            dashPage.calendarShouldBeVisible()
+
+            const day = Cypress.env('appointmentDay')
+            dashPage.selectDay(day)
+
+            dashPage.appointmentShouldBe(data.customer, data.appointmentHour)
+
+
 
             //cy.log(' O Id do Ramom é ' + Cypress.env('providerId') )
             
@@ -48,78 +57,3 @@ describe('dashboard', function () {
     })
 })
 
-import moment from 'moment'
-
-Cypress.Commands.add('createAppointment', function(){
-     let now = new Date()
-
-     now.setDate(now.getDate() + 1 )
-     const date = moment(now).format('YYYY-MM-DD  14:00:00 ')
-
-     const payload ={
-        provider_id: Cypress.env('providerId'),
-        date: date
-     }
-
-     cy.request({
-        method: 'POST',
-        url:'http://localhost:3333/appointments',
-        body: payload,
-        headers: {
-            authorization: 'Bearer ' + Cypress.env('apiToken')
-        }
-
-    }).then(function(response){
-        expect(response.status).to.eq(200)
-        
-
-    })
-})
-
-
-
-
-
-Cypress.Commands.add('setProviderId', function(providerEmail){
-    cy.request({
-        method: 'GET',
-        url: 'http://localhost:3333/providers',
-        headers: {
-            authorization: 'Bearer ' + Cypress.env('apiToken')
-        }
-    }).then(function(response){
-        expect(response.status).to.eq(200)
-        console.log(response.body)
-
-        const providerList = response.body
-
-        providerList.forEach(function(provider){
-            if (provider.email === providerEmail){
-                Cypress.env('providerId', provider.id)
-            }
-
-        })
-    })
-})
-
-
-
-Cypress.Commands.add('apiLogin',function(user){
-
-    const payload = {
-        email: user.email,
-        password: user.password
-    }
-
-    cy.request({
-        method: 'POST',
-        url:'http://localhost:3333/sessions',
-        body: payload,
-    }).then(function(response){
-        expect(response.status).to.eq(200)
-        Cypress.env('apiToken', response.body.token)
-
-    })
-
-
-})
--- cypress/support/pages/dash/elements.js
+++ cypress/support/pages/dash/elements.js
@@ -1,4 +1,7 @@
 exports.el = {
+    calendar: '.DayPicker',
+    boxDay: '.DayPicker-Day--available',
+    boxHour: 'span[class=appointment]'
 
     
 
